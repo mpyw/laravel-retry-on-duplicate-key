cc71e6e74f3735fc9ca113ae589efb28bad07a1f
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-11-18 14:19:45 +0900

    fix: 🐛 Wrap with if (false) { ... }

--- _ide_helper.php
+++ _ide_helper.php
@@ -2,29 +2,31 @@
 
 namespace Illuminate\Database
 {
-    interface ConnectionInterface
-    {
-        /**
-         * Retries once on duplicate key errors.
-         *
-         * @param mixed  ...$args
-         * @return mixed
-         * @see \Mpyw\LaravelRetryOnDuplicateKey\RetriesOnDuplicateKey
-         */
-        public function retryOnDuplicateKey(callable $callback, ...$args);
-    }
-
-    class Connection implements \Illuminate\Database\ConnectionInterface
-    {
-        /**
-         * Retries once on duplicate key errors.
-         *
-         * @param mixed  ...$args
-         * @return mixed
-         * @see \Mpyw\LaravelRetryOnDuplicateKey\RetriesOnDuplicateKey
-         */
-        public function retryOnDuplicateKey(callable $callback, ...$args)
+    if (false) {
+        interface ConnectionInterface
         {
+            /**
+             * Retries once on duplicate key errors.
+             *
+             * @param mixed  ...$args
+             * @return mixed
+             * @see \Mpyw\LaravelRetryOnDuplicateKey\RetriesOnDuplicateKey
+             */
+            public function retryOnDuplicateKey(callable $callback, ...$args);
+        }
+
+        class Connection implements \Illuminate\Database\ConnectionInterface
+        {
+            /**
+             * Retries once on duplicate key errors.
+             *
+             * @param mixed  ...$args
+             * @return mixed
+             * @see \Mpyw\LaravelRetryOnDuplicateKey\RetriesOnDuplicateKey
+             */
+            public function retryOnDuplicateKey(callable $callback, ...$args)
+            {
+            }
         }
     }
 }
