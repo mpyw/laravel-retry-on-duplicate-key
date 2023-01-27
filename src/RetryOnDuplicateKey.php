7882d0bd53be3aee2b6a8297e20d1edb483a0353
mpyw <ryosuke_i_628@yahoo.co.jp>
2023-01-27 21:31:20 +0900

    fix: 🐛 Add ability to recover on Postgres transactions

--- src/RetryOnDuplicateKey.php
+++ src/RetryOnDuplicateKey.php
@@ -6,6 +6,7 @@ namespace Mpyw\LaravelRetryOnDuplicateKey;
 
 use Illuminate\Database\Connection;
 use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\PostgresConnection;
 use Mpyw\LaravelUniqueViolationDetector\UniqueViolationDetector;
 use PDOException;
 
@@ -27,11 +28,11 @@ class RetryOnDuplicateKey
     public function __invoke(callable $callback, ...$args)
     {
         try {
-            return $callback(...$args);
+            return $this->withSavepoint(fn () => $callback(...$args));
         } catch (PDOException $e) {
             if ((new UniqueViolationDetector($this->connection))->violated($e)) {
                 $this->forceReferringPrimaryConnection();
-                return $callback(...$args);
+                return $this->withSavepoint(fn () => $callback(...$args));
             }
             throw $e;
         }
@@ -47,5 +48,28 @@ class RetryOnDuplicateKey
         if ($connection instanceof Connection) {
             $connection->recordsHaveBeenModified();
         }
+    }
+
+    /**
+     * @phpstan-template T
+     * @phpstan-param callable(): T $callback
+     * @phpstan-return T
+     * @return mixed
+     * @noinspection PhpDocMissingThrowsInspection
+     * @noinspection PhpUnhandledExceptionInspection
+     */
+    protected function withSavepoint(callable $callback)
+    {
+        return $this->needsSavepoint()
+            ? $this->connection->transaction(fn () => $callback())
+            : $callback();
+    }
+
+    protected function needsSavepoint(): bool
+    {
+        // In Postgres, savepoints allow recovery from errors.
+        // This ensures retrying should work also in transactions.
+        return $this->connection instanceof PostgresConnection
+            && $this->connection->transactionLevel() > 0;
     }
 }
